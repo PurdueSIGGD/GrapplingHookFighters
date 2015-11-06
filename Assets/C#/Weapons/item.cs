2b18209531f6d77828a8c105abbc75e3de14e8de
EverettG <egwyn@purdue.edu>
2015-11-05 21:48:19 -0500

    Renaming Files
    Reimplementing item interface and gun class. making the gun class part
    of the item interface

---
+++ Assets/C#/Weapons/item.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface item  {
+	
+	//when the weapon trigger is held
+	void click ();
+	//when the weapon trigger is released
+	void unclick ();
+}
