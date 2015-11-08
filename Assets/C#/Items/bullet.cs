be54eb61f7c9d0ed7a8eacab04df54407cc8f899
EverettG <egwyn@purdue.edu>
2015-11-07 22:13:48 -0500

    Almost have bullets Shooting
    bullets shoot with left mouse click
    still need to work on their starting position and their speed

---
+++ Assets/C#/Items/bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+	//Rigidbody2D bulletrigid2D;
+	//BoxCollider2D bulletBox2D;
+
+	// Use this for initialization
+	void Start () {
+	//	bulletBox2D = GetComponentInParent<BoxCollider2D>();
+	//	bulletrigid2D = GetComponentInParent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/C#/Items/gun.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gun : MonoBehaviour, item {
+
+	public bool trigger;
+	public float timeToShoot = 5;
+	private float timeSincelast = 0;
+	public Vector2 itemAngle;
+	//the point at which bullets come out of
+	public Vector2 shootpoint;
+	public Quaternion gunAngle;
+	public bullet kapeeeeewm;
+	public Vector2 reticlePos;
+	public GameObject playerID;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void click(){
+		trigger = true;
+	}
+
+	public void unclick(){
+		trigger = false;
+	}
+
+	//a send message command
+	public void SetPlayerID(){
+	//will set an ID number as an Int
+	}
+
+	// Update is called once per frame
+	void Update () {
+		shootpoint.x = this.gameObject.transform.position.x+1.02f;
+		shootpoint.y = this.gameObject.transform.position.y+0.05f;
+		gunAngle = GetComponentInParent<Transform> ().rotation;
+		//checked to see if there was a mouseplayer click
+		//this could be resource intensive as it is calling a method each update so the click()&unclick() method
+		//could be removed from the item interface
+		if (Input.GetMouseButtonDown (0)) {
+			click ();
+		} else {
+			unclick();
+		}
+		//update shooting
+		timeSincelast += Time.deltaTime;
+		if (trigger && (timeSincelast > timeToShoot)) {
+			kapeeeeewm = (bullet)Instantiate(kapeeeeewm,shootpoint,gunAngle);
+			reticlePos = GameObject.FindGameObjectWithTag("MainCamera").transform.FindChild("Reticle1").position;
+			Vector2 thing = reticlePos -shootpoint;
+			thing.Normalize();
+			kapeeeeewm.GetComponent<Rigidbody2D>().AddForce(thing*5f);
+			timeSincelast = 0;
+		}
+
+	}
+}
