ed53d875515165e8b336ecbc827c1403603e432e
JCJ0NES <theemailofjayce@gmail.com>
2015-11-14 21:09:57 -0500

    Spikes
    Made some spikes. They're terrible!

---
+++ Assets/C#/Hazard.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hazard : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.CompareTag ("Player")) {
+			col.SendMessage("Death",SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
