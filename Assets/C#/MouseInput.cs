85efee96940aa6806fc60eab98a9ad76c17d056d
Lonswaya <alonswa@purdue.edu>
2015-11-19 23:17:44 -0500

    Fixed errors and merge, need gamepad to finish aiming

--- Assets/C#/MouseInput.cs
+++ Assets/C#/MouseInput.cs
@@ -30,8 +30,10 @@ public class MouseInput : MonoBehaviour {
 		mousePosition = new Vector2[NUM_MICE];
 		lastMousePosition = new Vector2[NUM_MICE];
 		hasItem = new bool[NUM_MICE];
+		hasItem2 = new bool[NUM_MICE];
 		for(int i = 0; i< NUM_MICE;i++){
 			hasItem[i] = false;
+			hasItem2[i] = false;
 		}
 		
 	}
@@ -62,51 +64,87 @@ public class MouseInput : MonoBehaviour {
             if (mice[i - 1] == null) {
                 break;
             }
-
+			GameObject player = GameObject.Find("Player" + i);
+			GameObject reticle = GameObject.Find("Reticle" + i);
+			Transform center = player.transform.FindChild("Center");
 			Vector3 look;
 			Vector3 playerPos;
 			//print(i);
-			if (GameObject.Find("Player" + i) == null) return;
-			Transform playerTrans = GameObject.Find("Player" + i).transform;
-			playerPos = GameObject.Find("Player" + i).transform.position;
+			if (player == null) return;
+			Transform playerTrans = player.transform;
+			playerPos = player.transform.position;
+			if (player.GetComponent<player>().joystickController) {
+				look = new Vector3(Input.GetAxis("JoyX" + i), Input.GetAxis("JoyY" + i), 0);
+				reticle.transform.localPosition = Vector3.right;
+				center.transform.localEulerAngles = new Vector3(0,0,0);
+			} else {
+				look = new Vector3(mousePosition[i-1].x - lastMousePosition[i-1].x, mousePosition[i-1].y - lastMousePosition[i-1].y, 0);
+				Transform rectTrans = reticle.transform;
+				look = rectTrans.position + look;
+				look = playerTrans.TransformVector(look);
+				rectTrans.position = look;
+				//clamp distance
+				float dist = Vector3.Distance(rectTrans.position, playerPos);
+				if (dist > 2) {
+					rectTrans.position += (playerPos - rectTrans.position).normalized * (dist - 2);
+				}
+				center.LookAt(reticle.transform.position);
+				Vector3 rotation = new Vector3(0, 0, -center.localEulerAngles.x);
+				center.transform.localEulerAngles = rotation;
+				if(reticle.transform.position.x < player.transform.position.x) {
+					center.transform.localEulerAngles += new Vector3(0, 180, 0);
+				}
+			}
 			//look = new Vector3(mousePosition[i-1].x, mousePosition[i-1].y, playerPos.z);
-			look = new Vector3(mousePosition[i-1].x - lastMousePosition[i-1].x, mousePosition[i-1].y - lastMousePosition[i-1].y, 0);
+			//look = new Vector3(mousePosition[i-1].x - lastMousePosition[i-1].x, mousePosition[i-1].y - lastMousePosition[i-1].y, 0);
 			//look = new Vector3(mice[i-1].XDelta * Time.deltaTime, -mice[i-1].YDelta * Time.deltaTime, 0);
-			Transform rectTrans = GameObject.Find("Reticle" + i).transform;
-			look = rectTrans.position + look;
-			look = playerTrans.TransformVector(look);
-			rectTrans.position = look;
-			//clamp distance
-			float dist = Vector3.Distance(rectTrans.position, playerPos);
-			if (dist > 2) {
-				rectTrans.position += (playerPos - rectTrans.position).normalized * (dist - 2);
-			}
-						
-			GameObject.Find("Player" + i).transform.FindChild("Center").LookAt(GameObject.Find("Reticle" + i).transform.position);
-			Vector3 rotation = new Vector3(0, 0, -GameObject.Find("Player" + i).transform.FindChild("Center").localEulerAngles.x);
-			GameObject.Find("Player" + i).transform.FindChild("Center").transform.localEulerAngles = rotation;
-			if(GameObject.Find("Reticle" + i).transform.position.x < GameObject.Find("Player" + i).transform.position.x) {
-				GameObject.Find("Player" + i).transform.FindChild("Center").transform.localEulerAngles += new Vector3(0, 180, 0);
-			}
-			if (hasItem[i - 1]) {
-	            if ((bool) mice[i - 1].Buttons.GetValue(0)) {
-					if (GameObject.Find("Player" + i).transform.FindChild("Center").childCount > 0) {
-	                	GameObject.Find("Player" + i).transform.FindChild("Center").GetChild(0).SendMessage("click");
+
+
+
+			if (hasItem[i - 1] && center.GetChild(0).childCount > 0 && (center.GetChild (0).GetComponent<gun>() || center.GetChild (0).GetComponent<grenade>() || center.GetChild (0).GetComponent<PortalGun>())) {
+				if (player.GetComponent<player>().joystickController) {
+					if (Input.GetAxis("JFire" + i) > 0) {
+						center.GetChild (0).SendMessage ("click");
+					} else {
+						center.GetChild (0).SendMessage ("unclick");
 					}
-	            } else {
-					if (GameObject.Find("Player" + i).transform.FindChild("Center").childCount > 0) {
-	                	GameObject.Find("Player" + i).transform.FindChild("Center").GetChild(0).SendMessage("unclick");
+				} else {
+					if ((bool)mice [i - 1].Buttons.GetValue (0)) {
+						center.GetChild (0).SendMessage ("click");
+					} else {
+						center.GetChild (0).SendMessage ("unclick");
 					}
-	            }
+				}
 			}
-
-            if ((bool)mice[i - 1].Buttons.GetValue(1)) {
-                GameObject.Find("Player" + i).GetComponent<GrappleLauncher>().SendMessage("fire");
-            } else {
-                GameObject.Find("Player" + i).GetComponent<GrappleLauncher>().SendMessage("mouseRelease");
-            }
-
-
+			if (hasItem2[i - 1] && center.childCount > 1 && center.GetChild(1).childCount > 0 && !center.GetChild (1).GetComponent<player>()) {
+				if (player.GetComponent<player>().joystickController) {
+					if (Input.GetAxis("AltJFire" + i) > 0) {
+						center.GetChild (1).SendMessage ("click");
+					} else {
+						center.GetChild (1).SendMessage ("unclick");
+					}
+				} else {
+					if ((bool)mice [i - 1].Buttons.GetValue (1)) {
+						center.GetChild (1).SendMessage ("click");
+					} else {
+						center.GetChild (1).SendMessage ("unclick");
+					}
+			}
+				} else {
+				if (player.GetComponent<player>().joystickController) {
+					if (Input.GetAxis("AltJFire" + i) > 0) {
+						player.GetComponent<GrappleLauncher> ().SendMessage ("fire");
+					} else {
+						player.GetComponent<GrappleLauncher> ().SendMessage ("mouseRelease");
+					}
+				} else {
+					if ((bool)mice [i - 1].Buttons.GetValue (1)) {
+						player.GetComponent<GrappleLauncher> ().SendMessage ("fire");
+					} else {
+						player.GetComponent<GrappleLauncher> ().SendMessage ("mouseRelease");
+					}
+				}
+			}
             /*print(Vector3.Distance(playerPos, look));
             if (Vector3.Distance(playerPos, look) > 10) {
                 Vector3 distanceSet = (playerPos - look).normalized * 5;
