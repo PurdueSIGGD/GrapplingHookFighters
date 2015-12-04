f140e512b250d04c694805bf0154cb3fb897e22c
Daniel Hewlett <DanCHewlett@gmail.com>
2015-12-03 19:57:51 -0500

    Revert "Revert "Added FMOD""
    This reverts commit 5d285b0434576c1af671a99a5af70988259521b0.

---
+++ Assets/Plugins/FMOD/Editor/FMODEmitterGizmo.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+using UnityEditor;
+class FMODEmitterGizmo
+{
+    [DrawGizmo(GizmoType.Selected | GizmoType.Active)]
+    static void DrawGizmo(FMOD_StudioEventEmitter studioEmitter, GizmoType gizmoType)
+    {
+        if (studioEmitter.asset != null && studioEmitter.enabled &&
+            (!UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode || UnityEditor.EditorApplication.isPlaying))
+        {
+            FMOD.Studio.EventDescription desc = null;
+            desc = FMODEditorExtension.GetEventDescription(studioEmitter.asset.id);
+
+            if (desc != null)
+            {
+                float max, min;
+                desc.getMaximumDistance(out max);
+                desc.getMinimumDistance(out min);
+
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(studioEmitter.transform.position, min);
+                Gizmos.DrawWireSphere(studioEmitter.transform.position, max);
+            }
+        }
+    }
+}
+#endif
