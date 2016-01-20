8f036ce40036c1e7727a2461f7f616dc3e8cec1e
Daniel Hewlett <DanCHewlett@gmail.com>
2016-01-20 18:12:16 -0500

    FountainOfYou added
    Oh boy

---
+++ Assets/FountainOfYou.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Experimental.Director;
+
+public class FountainOfYou : MonoBehaviour {
+
+	bool activated = false;
+	public ParticleSystem particles;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (particles != null && activated == false && GetComponent<HeldItem> ().focus != null) {
+			activated = true;
+			StartFountain ();
+		}
+	}
+
+	void StartFountain() {
+		particles.Play();
+	}
+}
