8f56db8d2ace8dc34bafd7126008b65f4debddf5
Jaylooker <jaylookerwooga@gmail.com>
2016-02-17 21:11:54 -0500

    Added Landmines
    Script needs work. Calling explosion needed.

---
+++ Assets/C#/Items/LandMine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandMine : MonoBehaviour {
+
+	private bool willexplode = false;
+    
+    // Use this for initialization
+	void Start () {
+        
+	}
+	
+    void OncollisionEnter2d(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player" && willexplode == true )
+        {
+            GetComponent<ExplosionScript>();
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	    /*if ()
+        {
+            
+        }
+        */
+	}
+}
