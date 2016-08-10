5dd1fbb99007a5f0542506c02f110782e8c31136
Lonswaya <alonswa@purdue.edu>
2016-08-10 12:41:14 -0400

    Added scenes with new platform tech

---
+++ Assets/C#/MapPlatforms.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapPlatforms : MonoBehaviour {
+
+	public Sprite bottom_wall, mid_wall, top_wall;
+	public Sprite left_floor, mid_floor, right_floor;
+	public Sprite misc1, misc2, misc3;
+}
