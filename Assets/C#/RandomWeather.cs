d8aba23b7223d4ed513d329cce319c3622b8d8ab
EverettG <egwyn@purdue.edu>
2016-09-12 19:46:53 -0400

    Adding a Stage and changes to weather weapon
    blah blah blah

---
+++ Assets/C#/RandomWeather.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomWeather : MonoBehaviour {
+
+	public GameObject m;//meteor
+	public GameObject r;//rain
+	public GameObject l;//lighting
+	gun thisgun;
+	int weather;
+	// Use this for initialization
+	void Start () {
+		weather = Random.Range (0,3);
+		thisgun = gameObject.GetComponent<gun> ();
+		switch (weather) {
+		case 0:
+			thisgun.projectileGameObject = m;
+			break;
+		case 1:
+			thisgun.projectileGameObject = r;
+			break;
+		case 2:
+			thisgun.projectileGameObject = l;
+			break;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
