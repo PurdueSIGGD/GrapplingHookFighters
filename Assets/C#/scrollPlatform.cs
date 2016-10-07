5100bd34098d53708eb992a6c99fd39d40c3e075
EverettG <egwyn@purdue.edu>
2016-10-06 20:23:02 -0400

    Adding in Infinite Scroll Stage
    Still need to add in proper weapons for it. And I think It might have
    some slowdown. still needs work

---
+++ Assets/C#/scrollPlatform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrollPlatform : MonoBehaviour {
+
+	public GameObject platform;
+	Vector3 downby = new Vector3(0,-.07f,0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += downby;
+		if (this.transform.position.y <= -60) {
+			GameObject newplat = (GameObject)Instantiate(platform,new Vector3(transform.position.x,30,0),new Quaternion(0,0,0,0));
+				Destroy(gameObject);
+		}
+	}
+}
