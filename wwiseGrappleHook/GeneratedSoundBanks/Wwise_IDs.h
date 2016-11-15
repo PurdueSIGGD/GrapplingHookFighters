5a5ca03795cb790b66173b29e258f82083d577a1
lshanker <lshanke@purdue.edu>
2016-11-15 08:14:24 -0500

    Adds sound effects list and some wwise files

---
+++ wwiseGrappleHook/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BALL_COLLISION = 3162160547U;
+        static const AkUniqueID INTRO = 1125500713U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID JUMP_STOP = 1582776124U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
